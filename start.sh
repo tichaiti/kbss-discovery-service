48b8b30735917c388faa46ce60cca54074115cb8
nucklehead <pierevans@gmail.com>
2020-03-31 22:02:31 -0400

    Fixing sed

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 CONSUL_URL=${CONSUL_URL:-consul:8500}
 VAULT_URL=${VAULT_URL:-http://vault:8200}
 sed -i s/CONSUL_URL/${CONSUL_URL}/g data/gloo-system/default.yaml
-sed -i s|VAULT_URL|${VAULT_URL}|g data/gloo-system/default.yaml
+sed -i "s|VAULT_URL|${VAULT_URL}|g" data/gloo-system/default.yaml
 
 /usr/local/bin/discovery --dir=/data/
